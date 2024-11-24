cedd316533ad950bd678adf17f10345ec07a8cf3
SwthSawTheme <sawtheme1@gmail.com>
2024-11-24 10:35:09 -0300

    iniciante, problema extremamente basico resolvido

---
+++ beecrowd/Cpp/iniciante/1001.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+    int a;
+    int b;
+    int c;
+
+    std::cin >> a;
+    std::cin >> b;
+
+    c = a + b;
+
+    std::cout << "X = " << c << std::endl;
+
+    return 0;
+}
---
+++ beecrowd/Cpp/iniciante/build.bat
@@ -0,0 +1,3 @@
+@echo off
+g++ 1001.cpp -o bin\1001
+bin\1001.exe < entrada.txt
