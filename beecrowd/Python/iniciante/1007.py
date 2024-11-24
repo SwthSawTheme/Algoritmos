5006f92045c14bc420a72f6501932fada60764a2
SwthSawTheme <sawtheme1@gmail.com>
2024-11-24 09:38:56 -0300

    iniciante problema diferenca resolvido

---
+++ beecrowd/Python/iniciante/1007.py
@@ -0,0 +1,11 @@
+# Diferença
+
+# FORMULA diferenca = ((a * b) - (c * d))
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+dif = ((a * b) - (c * d))
+print(f"DIFERENCA = {dif}")
