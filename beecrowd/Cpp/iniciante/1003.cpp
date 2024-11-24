5ef6e4aada82618b1bf7e3060e46d3626eb7fb36
SwthSawTheme <sawtheme1@gmail.com>
2024-11-24 10:57:44 -0300

    iniciante, problema Soma Simples resolvido

---
+++ beecrowd/Cpp/iniciante/1003.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+
+// Soma Simples
+int main(){
+    int a, b, c;
+
+    std::cin >> a;
+    std::cin >> b;
+
+    c = a + b;
+
+    std::cout << "SOMA = " << c << std::endl;
+
+    return 0;
+}
